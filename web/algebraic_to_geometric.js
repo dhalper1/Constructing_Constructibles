a82381d6809c75906b5345f8359cab0a7af728ca
Brad Guesman <james_guesman@brown.edu>
2018-04-25 19:52:44 -0400

    add stencil for algebraic to geometric converter

---
+++ web/algebraic_to_geometric.js
@@ -0,0 +1,21 @@
+/******************************************************************************/
+/*************************** ALGEBRAIC TO GEOMETRIC ***************************/
+/******************************************************************************/
+
+
+/*
+ * Takes a parse tree (as specified in constructible_parse_tree.js) and
+ * converts it to a sequence of drawing instructions (as specified in the
+ * example in test_resources.js).
+ * @param tree: a parse tree representing a constructible number.
+ * @return: a list of drawing instructions to construct that number on the
+ * x-axis in the plane.
+ */
+function parse_tree_to_geometric(tree) {
+  // TODO: do it lol
+  return []
+}
+
+/******************************************************************************/
+/************************* END ALGEBRAIC TO GEOMETRIC *************************/
+/******************************************************************************/
