94bb670ea0954316210fb79efdc42e2138d22990
Brad Guesman <james_guesman@brown.edu>
2018-04-24 21:19:14 -0400

    added support for highlighting intercepts

--- web/constructible_numbers.js
+++ web/constructible_numbers.js
@@ -6,6 +6,7 @@ const CIRCLE = 0          // Step is a circle.
 const LINE = 1            // Step is a line.
 
 const amount_to_draw_init = 0.01;
+const intersection_size = 5;
 
 /******************************************************************************/
 /************************** END CODES AND CONSTANTS ***************************/
@@ -35,8 +36,8 @@ draw_speed = 0.02       // how fast to draw
 // coordinate system. This means we'll need a scale, an x offset, and a
 // y offset.
 
-size_scale = 10.0   // this is the scaling factor for x AND y,
-                    // so we preserve aspect ratio
+x_scale = 10.0      // scaling factor for x
+y_scale = 10.0      // scaling factor for y
 x_offset = 0.0      // offset for x---may be unnecessary
 y_offset = 0.0      // offset for y---may be unnecessary
 
@@ -45,7 +46,7 @@ y_offset = 0.0      // offset for y---may be unnecessary
 // @param x: the plane x-coordinate to transform.
 // @return: the transformed coordinate, in the window coordinate system.
 function trans_x(x) {
-  return (x + x_offset) * size_scale + width / 2.0
+  return (x + x_offset) * x_scale + width / 2.0
 }
 
 // Transform a coordinate on the plane y-axis to a coordinate on the window
@@ -53,7 +54,7 @@ function trans_x(x) {
 // @param y: the plane y-coordinate to transform.
 // @return: the transformed coordinate, in the window coordinate system.
 function trans_y(y) {
-  return (height / 2.0) - ((y  + y_offset) * size_scale)
+  return (height / 2.0) - ((y  + y_offset) * y_scale)
 }
 
 // Transform a coordinate on the window x-axis to a coordinate on the plane
@@ -61,7 +62,7 @@ function trans_y(y) {
 // @param x: the window x-coordinate to transform.
 // @return: the transformed coordinate, in the plane coordinate system.
 function inv_trans_x(x) {
-  return ((x - width / 2.0) / size_scale) - x_offset
+  return ((x - width / 2.0) / x_scale) - x_offset
 }
 
 // Transform a coordinate on the window y-axis to a coordinate on the plane
@@ -69,7 +70,7 @@ function inv_trans_x(x) {
 // @param x: the window y-coordinate to transform.
 // @return: the transformed coordinate, in the plane coordinate system.
 function inv_trans_y(y) {
-  return ((y - height / 2.0) / -size_scale) - y_offset
+  return ((y - height / 2.0) / -y_scale) - y_offset
 }
 
 // Updates the scale of the graph to include a point. If the point is already
@@ -79,17 +80,21 @@ function inv_trans_y(y) {
 // @return: nothing.
 function update_scale(x, y) {
   if (steps[current_step].x_int > inv_trans_x(width)) {
-    size_scale = abs(2.1 * steps[current_step].x_int / width)
+    x_scale = abs(0.95 * (width / 2.0) / (steps[current_step].x_int + x_offset))
+    y_scale = x_scale
     redraw = true
   } else if (steps[current_step].x_int < inv_trans_x(0)) {
-    size_scale = abs(2.1 * steps[current_step].x_int / width)
+    x_scale = abs(0.95 * (width / 2.0) / (steps[current_step].x_int + x_offset))
+    y_scale = x_scale
     redraw = true
   }
   if (steps[current_step].y_int < inv_trans_y(height)) {
-    size_scale = abs(2.1 * steps[current_step].y_int / height)
+    y_scale = abs(0.95 * (height / 2.0) / (y + y_offset))
+    x_scale = y_scale
     redraw = true
   } else if (steps[current_step].y_int > inv_trans_y(0)) {
-    size_scale = abs(2.1 * steps[current_step].y_int / height)
+    y_scale = abs(0.95 * (height / 2.0) / (y + y_offset))
+    x_scale = y_scale
     redraw = true
   }
 }
@@ -128,15 +133,15 @@ steps = [
     x0: 8,
     y0: -1,
     r: 7,
-    x_int: 1000,
+    x_int: 400,
     y_int: 2,
     amount_to_draw: amount_to_draw_init
   },
   {
     type: CIRCLE,
-    x0: 400,
+    x0: 300,
     y0: 3,
-    r: 12,
+    r: 40,
     x_int: 20,
     y_int: 2,
     amount_to_draw: amount_to_draw_init
@@ -174,10 +179,11 @@ function draw() {
   // If we're currently working on a step, continue drawing it.
   if (current_step < steps.length) {
     if (steps[current_step].type == CIRCLE) {
+      noFill()
       stroke(130, 240, 180)
       arc(trans_x(steps[current_step].x0), trans_y(steps[current_step].y0),
-                  size_scale * steps[current_step].r * 2,
-                  size_scale * steps[current_step].r * 2,
+                  x_scale * steps[current_step].r * 2,
+                  y_scale * steps[current_step].r * 2,
                   0, steps[current_step].amount_to_draw * 2 * PI)
     } else {
       stroke(130, 240, 180)
@@ -209,14 +215,19 @@ function draw_entire_scene() {
   for (i = 0; i < current_step; i++) {
     step = steps[i]
     if (step.type == CIRCLE) {
+      noFill()
       stroke(130, 240, 180)
       ellipse(trans_x(step.x0), trans_y(step.y0),
-                  size_scale * step.r * 2, size_scale * step.r * 2)
+                  x_scale * step.r * 2, y_scale * step.r * 2)
     } else {
       stroke(130, 240, 180)
       line(0, trans_y(step.m * inv_trans_x(0) + step.b),
             width, trans_y(step.m * inv_trans_x(width) + step.b))
     }
+    fill(240, 20, 40)
+    noStroke()
+    ellipse(trans_x(step.x_int), trans_y(step.y_int),
+              intersection_size, intersection_size)
   }
   redraw = false
 }
