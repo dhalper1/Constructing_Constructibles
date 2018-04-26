c28ccf74a2b7e7b7917e606072a80cab0d678cea
dhalper1 <david_halpern@brown.edu>
2018-04-26 15:00:18 -0400

    Cleaning up the code.  Adding a few comments.

--- web/constructible_parse_tree.js
+++ web/constructible_parse_tree.js
@@ -1,12 +1,3 @@
-// input_val = "(+ 2 (/ 1 sqrt(3)))"
-
-// class Leaf {
-// 	constructor(num) {
-// 		this.num = num
-// 		this.is_leaf = true
-// 	}
-// }
-
 class Operation {
     constructor(operator, l_child, r_child) {
         this.operator = operator; // String representing the operator
@@ -17,40 +8,18 @@ class Operation {
 }
 
 class Tree {
-    // static operator_set = ["+", "-", "*", "/"];
-    // static operator_set;
     constructor(input) {
-        // this.operator_set =
-        this.operator_set = new Set(["+", "-", "*", "/"])
-        // this.input = input;
-        // console.log(this.is_valid(input));
+        this.operator_set = new Set(["+", "-", "*", "/"]);
         this.root = this.is_valid(input) ? this.parse(input) : null
     }
 
-    // find_substring(str, start) {
-    // 	i = start
-    // 	if str[start] == "(" {
-    // 		counter = 0
-    // 		i = 2
-    // 		while str[i] != ")" {
-    // 			if (str[i] == "(") {
-    // 				counter += 1
-    // 			}
-    // 			i += 1
-    // 		}
-    // 		while counter > 0 {
-    // 			if (str[i] == ")") {
-    // 				counter -= 1
-    // 			}
-    // 			i += 1
-    // 		}
-    // 		return [str[start:i], i]
-    // 	} else {
-    // 		return str[start], i + 1
-    // 	}
-    // }
-
-    find_substring(str, start) {
+    /**
+     * Helper method for parse
+     * @param str - The string
+     * @param start - The starting location for where to start finding the string to return inside of str
+     * @returns {*[]} - A tuple the substring and the returning index
+     */
+    _find_substring(str, start) {
         let i = start;
         while (str[start] === " ") {
             start += 1
@@ -65,43 +34,34 @@ class Tree {
                 counter -= str[i] === ")" ? 1 : 0;
                 i += 1
             }
-            // return [str.substring(start, i), i]
         } else {
-            // while (!this.operator_set.has(str[i])) {
-            //     i++
-            // }
             while (str[i] !== " " && str[i] !== ")" && !this.operator_set.has(str[i]) && i < str.length) {
                 i++
             }
-            // return [str.substr]
-            // return [str[start], i + 1]
         }
         return [str.substring(start, i), i + 1]
     }
 
+    /**
+     * Recursive parsing algorithm
+     * @param input The string
+     * @returns {Operation} The Operation for that given input
+     */
     parse(input) {
         input = input.trim();
-        // console.log(input);
         if ((input !== null) && (input[0] === "(") && (input.length >= 2)) {
-            const left_sub = this.find_substring(input, 3);
+            const left_sub = this._find_substring(input, 3);
             const left_to_parse = left_sub[0];
             let i = left_sub[1];
-            // console.log()
-            const right_sub = this.find_substring(input, i);
+            const right_sub = this._find_substring(input, i);
             const right_to_parse = right_sub[0];
             console.log(input);
             console.log(left_sub);
             console.log(right_sub);
 
-            // i = right_sub[1];
-
             return new Operation(input[1], this.parse(left_to_parse), this.parse(right_to_parse))
+
         } else if (input.startsWith("sqrt(")) {
-            // if (this.operator_set.has(input[5])) {
-            //     return new Operation("sqrt", new Operation(parseInt(input[5]), null))
-            // } else {
-            //     return new Operation("sqrt", this.parse(input[4]), null) // TODO NOTE THIS LOOKS WRONG SHOULD MAYBE BE 5 CAUSE NOT IN OPERATOR_SET
-            // }
             let i = 5;
             while (input[i] === " ") {
                 i++
@@ -112,17 +72,17 @@ class Tree {
                 // Notice that we cut off the final parentheses here
                 return new Operation("sqrt", new Operation(parseInt(input.substring(i, input.length - 1)), null, null))
             }
-            // let substr = input.substring(i, input.length - 1) // Get's rid of closing parentheses
-            // if (this.operator_set.has(substr[0])) {
-            //     return new Operation("sqrt", new Operation())
-            // }
 
         } else if (input !== "") {
-            // console.log(parseInt(input));
             return new Operation(parseInt(input), null, null)
         }
     }
 
+    /**
+     * Tests whether a string is valid or not
+     * @param input_val - The string
+     * @returns {boolean|*} - A boolean telling us whether or not we got valid input
+     */
     is_valid(input_val) {
         return this._check_parens(input_val) && this._check_legal_characters(input_val) &&
             this._check_infix(input_val);
@@ -150,7 +110,8 @@ class Tree {
             if (!legal_set.has(input_val[i])) {
                 let substr = input_val.substring(i, input_val.length);
                 // console.log(substr)
-                if (!(substr.startsWith("sqrt") || substr.startsWith("qrt") || substr.startsWith("rt") || substr.startsWith("t"))) {
+                if (!(substr.startsWith("sqrt") || substr.startsWith("qrt") ||
+                        substr.startsWith("rt") || substr.startsWith("t"))) {
                     return false
                 }
             }
@@ -159,12 +120,10 @@ class Tree {
     }
 
     _check_infix(input_val) {
-        // return false;
         for (let i = 0; i < input_val.length - 1; i++) {
             let char = input_val[i];
             if (char === "(") {
                 if (i + 1 < input_val.length) {
-                    // if (!this.operator_set.has(input_val[i + 1])) {
                     if (!this.operator_set.has(input_val[i + 1])) {
                         if (i >= 1 && input_val[i - 1] !== 't') {
                             return false
